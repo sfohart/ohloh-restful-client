501a7544de06a06ccd5933c66327c8b9bb30c3f0
Leandro Soriano Ferreira <sfohart@gmail.com>
2013-09-04 12:20:40 -0300

    reimplementando o modo como os dados sao baixados. ainda com problemas

--- ohloh-restful-client/src/main/java/br/ufba/dcc/mestrado/computacao/service/impl/OhLohStackServiceImpl.java
+++ ohloh-restful-client/src/main/java/br/ufba/dcc/mestrado/computacao/service/impl/OhLohStackServiceImpl.java
@@ -1,5 +1,6 @@
 package br.ufba.dcc.mestrado.computacao.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -7,6 +8,7 @@ import javax.inject.Inject;
 import br.ufba.dcc.mestrado.computacao.ohloh.data.project.OhLohProjectDTO;
 import br.ufba.dcc.mestrado.computacao.ohloh.data.stack.OhLohStackDTO;
 import br.ufba.dcc.mestrado.computacao.ohloh.entities.project.OhLohProjectEntity;
+import br.ufba.dcc.mestrado.computacao.ohloh.entities.project.OhLohTagEntity;
 import br.ufba.dcc.mestrado.computacao.ohloh.entities.stack.OhLohStackEntity;
 import br.ufba.dcc.mestrado.computacao.ohloh.entities.stack.OhLohStackEntryEntity;
 import br.ufba.dcc.mestrado.computacao.ohloh.restful.client.OhLohRestfulClient;
@@ -56,6 +58,8 @@ public class OhLohStackServiceImpl extends BaseOhLohServiceImpl<OhLohStackDTO, L
 		OhLohBaseRequest request  = new OhLohBaseRequest();
 		
 		if (entity.getOhLohStackEntries() != null) {
+			List<OhLohTagEntity> totalTagList = new ArrayList<>();
+			
 			for (OhLohStackEntryEntity stackEntry : entity.getOhLohStackEntries()) {
 				if (stackEntry.getProjectId() != null) {
 					OhLohProjectEntity project = projectService.findById(stackEntry.getProjectId());
@@ -64,12 +68,36 @@ public class OhLohStackServiceImpl extends BaseOhLohServiceImpl<OhLohStackDTO, L
 						OhLohProjectDTO projectDTO = restfulClient.getProject(stackEntry.getProjectId().toString(), request);
 						project = projectService.buildEntity(projectDTO);
 						projectService.validateEntity(project);
+				
+						if (project != null && project.getOhLohTags() != null) {
+							totalTagList.addAll(project.getOhLohTags());
+						}
 						
 						stackEntry.setProject(project);
 					}
 				}
 			}
+			
+			for (OhLohStackEntryEntity stackEntry : entity.getOhLohStackEntries()) {
+				if (stackEntry.getProject() != null && stackEntry.getProject().getOhLohTags() != null) {
+					
+					List<OhLohTagEntity> projectTagList = new ArrayList<>();
+					
+					for (OhLohTagEntity tag : stackEntry.getProject().getOhLohTags()) {
+						if (totalTagList.contains(tag)) {
+							int indexOf = totalTagList.indexOf(tag);
+							projectTagList.add(totalTagList.get(indexOf));
+						}
+					}
+					
+					stackEntry.getProject().getOhLohTags().clear();
+					stackEntry.getProject().getOhLohTags().addAll(projectTagList);
+				}				
+			}
+			
 		}
+		
+		
 		
 	}
 	
